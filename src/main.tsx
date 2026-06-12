223bfb0d50bf3a056a8ca2431417ce2546d7461a
Sanghyug Park <parkihs@hanmail.net>
2026-06-12 14:26:03 +0900

    Add Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
+import ReactGA from "react-ga4";
 
 import "./index.css";
 import App from "./App.tsx";
+
+const GA_ID = import.meta.env.VITE_GA_ID;
+
+if (GA_ID) {
+  ReactGA.initialize(GA_ID);
+}
 
 registerSW({
   immediate: true,
